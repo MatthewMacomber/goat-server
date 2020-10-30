e2387cd7d60756fddb5186815e655ac192f61256
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-30 12:30:05 -0700

    seed fix

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,24 +1,22 @@
 BEGIN;
 
 TRUNCATE
-  "goals",
-  "rewards",
-  "user";
+  'goals',
+  'rewards',
+  'user';
 
-INSERT INTO "user" ("id", "username", "name", "password")
+INSERT INTO 'user' ('username', 'name', 'password')
 VALUES
   (
-    1,
     'admin',
     'Goat Admin',
-    -- password = "pass"
+    -- password = 'pass'
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
-INSERT INTO "goals" ("id", "user_id", "title", "description", "points", "end_date", "complete", "archive")
+INSERT INTO 'goals' ('user_id', 'title', 'description', 'points', 'end_date', 'complete', 'archive')
 VALUES
   (
-    1, 
     1,
     'make my bed', 
     'I will make my bed every morning before leaving for work', 
